8770dfdf4a5b0ffbacd98dfc0c19268a4a91e708
heyMadhur <madhurgupta12112002@gmail.com>
2024-06-16 20:00:08 +0530

    Required Changes Done, Working fine locally

--- app.py
+++ app.py
@@ -1,12 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, request, jsonify
+from Product.Required_JSON import call_api
+from Health_Score.Health_Score_Generator import evaluate_health
 
 app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "The MBSA Server is Running"
+    return "The KYC Server is Running"
 
-@app.route("/mbsa")
-def mbsa():
-    return render_template('index.html')
+@app.route("/get_product_info", methods=['GET'])
+def get_product_info():
+    barcode= request.args.get('barcode')
+    if not barcode:
+        return jsonify({'error': 'Barcode not provided'}), 400
+    
+    print("Fetching Product Info")
+    response= call_api(barcode)
+    print("WE ARE PRINTING")
+    
+    if response:
+        Health_Score, eco_grade, explanation, def_nutri, suf_nutri, exc_nutri = evaluate_health(response)
+        return jsonify({
+            'Health Score': Health_Score,
+            'Grade': eco_grade.capitalize(),
+            'Nutrient Analysis': {
+                'Deficient Nutrients': def_nutri,
+                'Sufficient Nutrients': suf_nutri,
+                'Excessive Nutrients': exc_nutri
+            },
+            'Inference': explanation
+        })
+    
+    else:
+        # This case should not occur because call_api handles the None case and returns a tuple with an error message
+        return jsonify({'error': 'Unexpected error occurred'}), 500
+    
+if __name__ == '__main__':
+    app.run(debug=True)
 
