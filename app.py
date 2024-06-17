f9cbe5c1d1d4822aade69fbbbfc865913be47f07
heyMadhur <madhurgupta12112002@gmail.com>
2024-06-17 08:37:11 +0530

    2 enbdpoints created for product details and product score

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from Product.Required_JSON import call_api
 from Health_Score.Health_Score_Generator import evaluate_health
+import json
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
@@ -21,8 +22,25 @@ def get_product_info():
     print("WE ARE PRINTING")
     
     if response:
+        return response
+    
+    else:
+        # This case should not occur because call_api handles the None case and returns a tuple with an error message
+        return jsonify({'error': 'Unexpected error occurred'}), 500
+    
+@app.route("/get_product_score", methods=['GET'])
+def get_product_score():
+    barcode= request.args.get('barcode')
+    if not barcode:
+        return jsonify({'error': 'Barcode not provided'}), 400
+    
+    print("Fetching Product Info")
+    response= call_api(barcode)
+    print("WE ARE PRINTING")
+    
+    if response:
         Health_Score, eco_grade, explanation, def_nutri, suf_nutri, exc_nutri = evaluate_health(response)
-        return jsonify({
+        return jsonify ({'rank':{
             'Health Score': Health_Score,
             'Grade': eco_grade.capitalize(),
             'Nutrient Analysis': {
@@ -31,7 +49,8 @@ def get_product_info():
                 'Excessive Nutrients': exc_nutri
             },
             'Inference': explanation
-        })
+        }})
+        
     
     else:
         # This case should not occur because call_api handles the None case and returns a tuple with an error message
